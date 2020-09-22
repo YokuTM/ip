19451295ad827e6ca56306c9c23f5f332047ac9a
Vlad <58938182+YokuTM@users.noreply.github.com>
2020-09-23 00:06:52 +0500

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     echo($d);
 ?>
 <ul id="nav">
-    <li><a href="laba1-5.php">lab_1_1</a>
-    <li><a href="lab-2-10.php">lab_1_2</a>
-    <li><a href="lab2-11.php">lab_1_3</a>
+    <li><a href="laba1-5.php">lab1-1</a>
+    <li><a href="lab-2-10.php">lab1-2</a>
+    <li><a href="lab2-11.php">lab1-3</a>
 </ul>

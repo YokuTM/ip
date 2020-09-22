2be297c26bc2758c03695eb943321dfb04db49f8
Vlad <58938182+YokuTM@users.noreply.github.com>
2020-09-23 00:38:08 +0500

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,6 @@
 <?php
     $d=date("d.m.Y H:i");
     echo($d);
-    echo "<img src='https://yt3.ggpht.com/a/AATXAJxgFA7Gy1y4szVT9zNWCIMWO4m0QvHuA1K9qCiNlA=s900-c-k-c0xffffffff-no-rj-mo'>";
 ?>
 <ul id="nav">
     <li><a href="lab1-5-2.php">lab1-1</a>
@@ -11,3 +10,4 @@
     <li><a href="lab-2-10.php">lab1-2</a>
     <li><a href="lab2-11.php">lab1-3</a>
 </ul>
+

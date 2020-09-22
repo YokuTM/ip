2be60382cafbebd5b1e8ba494c1a28adbb054f07
Vlad <58938182+YokuTM@users.noreply.github.com>
2020-09-23 00:05:44 +0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<p> Дата и время:
+<p>
+<?php
+    $d=date("d.m.Y H:i");
+    echo($d);
+?>
+<ul id="nav">
+    <li><a href="laba1-5.php">lab_1_1</a>
+    <li><a href="lab-2-10.php">lab_1_2</a>
+    <li><a href="lab2-11.php">lab_1_3</a>
+</ul>

d64a19d6dcd34737b9e6b97c274d3645df13e2c4
Vlad <58938182+YokuTM@users.noreply.github.com>
2020-09-23 00:01:24 +0500

    Add files via upload

---
+++ lab-2-10.php
@@ -0,0 +1,131 @@
+<p> Задача 2:
+<p>
+<html>
+	<style type="text/css">
+	td.col1 { color: red; }
+	</style>
+	<body style='font-size:20px ; font-weight:100; font-family: Geneva, Times New Roman, Helvetica, sans-serif;'>
+		<table border="1" width="10">
+			<tr align="center">
+			<td>1</td>
+			<td class="col1">2</td>
+			<td>3</td>
+			<td class="col1">4</td>
+			<td>5</td>
+			<td class="col1">6</td>
+			<td>7</td>
+			<td class="col1">8</td>
+			<td>9</td>
+			<td class="col1">10</td>
+			<tr/>
+			<tr align="center">
+			<td>11</td>
+			<td class="col1">12</td>
+			<td>13</td>
+			<td class="col1">14</td>
+			<td>15</td>
+			<td class="col1">16</td>
+			<td>17</td>
+			<td class="col1">18</td>
+			<td>19</td>
+			<td class="col1">20</td>
+			<tr/>
+			<tr align="center">
+			<td>21</td>
+			<td class="col1">22</td>
+			<td>23</td>
+			<td class="col1">24</td>
+			<td>25</td>
+			<td class="col1">26</td>
+			<td>27</td>
+			<td class="col1">28</td>
+			<td>29</td>
+			<td class="col1">30</td>
+			<tr/>
+			<tr align="center">
+			<td>31</td>
+			<td class="col1">32</td>
+			<td>33</td>
+			<td class="col1">34</td>
+			<td>35</td>
+			<td class="col1">36</td>
+			<td>37</td>
+			<td class="col1">38</td>
+			<td>39</td>
+			<td class="col1">40</td>
+			<tr/>
+			<tr align="center">
+			<td>41</td>
+			<td class="col1">42</td>
+			<td>43</td>
+			<td class="col1">44</td>
+			<td>45</td>
+			<td class="col1">46</td>
+			<td>47</td>
+			<td class="col1">48</td>
+			<td>49</td>
+			<td class="col1">50</td>
+			<tr/>
+			<tr align="center">
+			<td>51</td>
+			<td class="col1">52</td>
+			<td>53</td>
+			<td class="col1">54</td>
+			<td>55</td>
+			<td class="col1">56</td>
+			<td>57</td>
+			<td class="col1">58</td>
+			<td>59</td>
+			<td class="col1">60</td>
+			<tr/>
+			<tr align="center">
+			<td>61</td>
+			<td class="col1">62</td>
+			<td>63</td>
+			<td class="col1">64</td>
+			<td>65</td>
+			<td class="col1">66</td>
+			<td>67</td>
+			<td class="col1">68</td>
+			<td>69</td>
+			<td class="col1">70</td>
+			<tr/>
+			<tr align="center">
+			<td>71</td>
+			<td class="col1">72</td>
+			<td>73</td>
+			<td class="col1">74</td>
+			<td>75</td>
+			<td class="col1">76</td>
+			<td>77</td>
+			<td class="col1">78</td>
+			<td>79</td>
+			<td class="col1">80</td>
+			<tr/>
+			<tr align="center">
+			<td>81</td>
+			<td class="col1">82</td>
+			<td>83</td>
+			<td class="col1">84</td>
+			<td>85</td>
+			<td class="col1">86</td>
+			<td>87</td>
+			<td class="col1">88</td>
+			<td>89</td>
+			<td class="col1">90</td>
+			<tr/>
+			<tr align="center">
+			<td>91</td>
+			<td class="col1">92</td>
+			<td>93</td>
+			<td class="col1">94</td>
+			<td>95</td>
+			<td class="col1">96</td>
+			<td>97</td>
+			<td class="col1">98</td>
+			<td>99</td>
+			<td class="col1">100</td>
+			<tr/>
+		</table>
+	</body>
+</html>
